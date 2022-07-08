f351b010676706b4dd50cc50979ba5c411d9b7fb
Alessandro Riva <alex27riva@gmail.com>
2022-07-08 17:02:07 +0200

    Profile dialog

--- lib/profile.dart
+++ lib/profile.dart
@@ -2,29 +2,139 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'images.dart' as images;
 
-class Profile extends StatelessWidget {
+class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
 
   @override
+  State<Profile> createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  final currentPassController = TextEditingController();
+  final newPassController = TextEditingController();
+  final newPassRepeatController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    Future openDialog() => showDialog(
+    void changePassword() {
+      Navigator.of(context).pop();
+    }
+
+    void changeEmail() {
+      Navigator.of(context).pop();
+    }
+
+    // Change password dialog
+    Future openChangePasswordDialog() => showDialog(
           context: context,
           builder: (context) => AlertDialog(
             title: const Text('Cambio password'),
-            content: const TextField(
-              autofocus: true,
-              decoration: InputDecoration(hintText: 'Enter '),
+            content: SizedBox(
+              height: 200,
+              child: Column(
+                children: [
+                  TextField(
+                    autofocus: true,
+                    decoration: const InputDecoration(
+                        hintText: 'Inserisci la password attuale'),
+                    controller: currentPassController,
+                  ),
+                  TextField(
+                    decoration:
+                        const InputDecoration(hintText: 'Inserisci la nuova password'),
+                    controller: newPassController,
+                  ),
+                  TextField(
+                    decoration: const InputDecoration(hintText: 'Ripeti password'),
+                    controller: newPassRepeatController,
+                  ),
+                ],
+              ),
             ),
             actions: [
-              TextButton(onPressed: () {}, child: const Text('Annulla')),
-              TextButton(onPressed: () {}, child: const Text('Conferma')),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Annulla')),
+              TextButton(
+                  onPressed: () {
+                    changeEmail();
+                  },
+                  child: const Text('Conferma')),
+            ],
+          ),
+        );
+
+    // Change email dialog
+    Future openChangeEmailDialog() => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Cambio email', textAlign: TextAlign.center),
+            //contentPadding: const EdgeInsets.fromLTRB(24.0, 0.0, 24.0, 24.0),
+            content: SizedBox(
+              height: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    autofocus: true,
+                    decoration: const InputDecoration(
+                        hintText: 'Inserisci la nuova email'),
+                    controller: currentPassController,
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Annulla')),
+              TextButton(
+                  onPressed: () {
+                    changePassword();
+                  },
+                  child: const Text('Conferma')),
+            ],
+          ),
+        );
+
+    // Delete profile dialog
+    Future openDeleteProfileDialog() => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Eliminazione profilo'),
+            content: SizedBox(
+              height: 100,
+              child: Column(
+                children: const [
+                  Text("Sei sicuro di voler eliminare l'account?")
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('Annulla')),
+              TextButton(
+                  onPressed: () {
+                    changePassword();
+                  },
+                  child: const Text(
+                    'Elimina',
+                    style: TextStyle(color: Colors.red),
+                  )),
             ],
           ),
         );
 
     return Scaffold(
         body: Container(
-            padding: const EdgeInsets.all(16.0),
+            margin: const EdgeInsets.all(16.0),
             child: Column(children: <Widget>[
               Container(
                   margin: const EdgeInsets.only(bottom: 16.0),
@@ -53,43 +163,62 @@ class Profile extends StatelessWidget {
                                 decoration: TextDecoration.none))),
                   ])),
               Container(
-                margin: const EdgeInsets.only(top: 100.0),
-                child: Center(
-                    child: Column(
-                  children: <Widget>[
-                    RichText(
-                        text: TextSpan(
-                      text: 'Cambia password',
-                      style: const TextStyle(
-                        fontSize: 20,
-                        color: Colors.black,
-                      ),
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = () => openDialog(),
-                    )),
-                    const Divider(
-                        color: Colors.black,
-                        height: 20,
-                        thickness: 1,
-                        indent: 20,
-                        endIndent: 10),
-                    const Text('Cambia email', style: TextStyle(fontSize: 20)),
-                    const Divider(
-                        color: Colors.black,
-                        height: 20,
-                        thickness: 1,
-                        indent: 20,
-                        endIndent: 10),
-                    const Text('Elimina profilo',
-                        style: TextStyle(color: Colors.red, fontSize: 20)),
-                    const Divider(
-                        color: Colors.black,
-                        height: 20,
-                        thickness: 1,
-                        indent: 20,
-                        endIndent: 10),
-                  ],
-                )),
+                margin: const EdgeInsets.only(top: 80.0),
+                child: Flexible(
+                  child: Center(
+                      child: Column(
+                    children: <Widget>[
+                      RichText(
+                          text: TextSpan(
+                        text: 'Cambia password',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () => openChangePasswordDialog(),
+                      )),
+                      const Divider(
+                          color: Colors.black,
+                          height: 20,
+                          thickness: 1,
+                          indent: 20,
+                          endIndent: 10),
+                      RichText(
+                          text: TextSpan(
+                        text: 'Cambia email',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () => openChangeEmailDialog(),
+                      )),
+                      const Divider(
+                          color: Colors.black,
+                          height: 20,
+                          thickness: 1,
+                          indent: 20,
+                          endIndent: 10),
+                      RichText(
+                          text: TextSpan(
+                            text: 'Elimina profilo',
+                            style: const TextStyle(
+                              fontSize: 20,
+                              color: Colors.red,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () => openDeleteProfileDialog(),
+                          )),
+                      const Divider(
+                          color: Colors.black,
+                          height: 20,
+                          thickness: 1,
+                          indent: 20,
+                          endIndent: 10),
+                    ],
+                  )),
+                ),
               ),
             ])));
   }
