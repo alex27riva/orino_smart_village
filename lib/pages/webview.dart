c41c138f98341da685508470a8a338069b382c10
Alessandro Riva <alex27riva@gmail.com>
2022-10-06 19:04:58 +0200

    Add menu in WebView

--- lib/pages/webview.dart
+++ lib/pages/webview.dart
@@ -1,11 +1,15 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:orino_smart_village/utils/utils.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class WebViewPage extends StatefulWidget {
   final String url;
   final String title;
-  const WebViewPage({Key? key, this.url = '', this.title='Browser'}) : super(key: key);
+
+  const WebViewPage({Key? key, this.url = '', this.title = 'Browser'})
+      : super(key: key);
 
   @override
   State<WebViewPage> createState() => _WebViewPageState();
@@ -18,9 +22,23 @@ class _WebViewPageState extends State<WebViewPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
+      appBar: AppBar(title: Text(widget.title), actions: <Widget>[
+        PopupMenuButton(
+          itemBuilder: (context) {
+            return [
+              PopupMenuItem<int>(
+                value: 0,
+                child: Text(AppLocalizations.of(context)!.openExternalBrowser),
+              ),
+            ];
+          },
+          onSelected: (value) {
+            if (value == 0) {
+              Utils.openUrl(widget.url);
+            }
+          },
+        ),
+      ]),
       body: WebView(
         initialUrl: widget.url,
         javascriptMode: JavascriptMode.unrestricted,
