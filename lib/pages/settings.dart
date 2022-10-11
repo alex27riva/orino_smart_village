75b744e84a9ea72444dcd76694e5530b154d08a4
Alessandro Riva <alex27riva@gmail.com>
2022-10-11 21:37:17 +0200

    Better settings code

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dropdown/flutter_dropdown.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+const double borderWidth = 1;
+
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
 
@@ -12,100 +14,64 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   bool darkThemeState = false;
   bool notificationState = false;
-  String _chosenValue = '';
 
   @override
   Widget build(BuildContext context) {
-    const double lineThickness = 0.7;
     return Scaffold(
         appBar: AppBar(
           title: Text(AppLocalizations.of(context)!.settingsTitle),
         ),
         body: Container(
-          margin: const EdgeInsets.all(20.0),
+          margin: const EdgeInsets.all(32.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               // Language
-
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(AppLocalizations.of(context)!.languageText),
-                      DropDown(
-                        items: const ["Italiano", "English"],
-                        hint: const Text("Italiano"),
-                        icon: const Icon(
-                          Icons.expand_more,
-                          color: Colors.blue,
-                        ),
-                        onChanged: print,
-                      ),
-                    ],
+              Container(
+                margin: const EdgeInsets.only(top: 15.0),
+                child: ListTile(
+                  shape: const Border(
+                      bottom:
+                          BorderSide(color: Colors.black, width: borderWidth)),
+                  leading: Text(AppLocalizations.of(context)!.languageText),
+                  trailing: DropDown(
+                    items: const ["Italiano", "English"],
+                    hint: const Text("Italiano"),
+                    icon: const Icon(
+                      Icons.expand_more,
+                      color: Colors.blue,
+                    ),
+                    onChanged: print,
                   ),
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Divider(
-                        color: Colors.black,
-                        height: 20,
-                        thickness: lineThickness,
-                        indent: 20,
-                        endIndent: 10),
-                  ),
-                ],
+                ),
               ),
-
-              // Dark theme
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(AppLocalizations.of(context)!.themeText),
-                      Switch(
-                          value: darkThemeState,
-                          onChanged: (newValue) {
-                            setState(() => darkThemeState = newValue);
-                          })
-                    ],
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Divider(
-                        color: Colors.black,
-                        height: 20,
-                        thickness: lineThickness,
-                        indent: 20,
-                        endIndent: 10),
-                  ),
-                ],
+              // notifications
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: SwitchListTile(
+                  title: Text(AppLocalizations.of(context)!.notificationText),
+                  value: notificationState,
+                  shape: const Border(
+                      bottom:
+                          BorderSide(color: Colors.black, width: borderWidth)),
+                  onChanged: (value) {
+                    setState(() => notificationState = value);
+                  },
+                ),
               ),
-              // Notifiche
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Text(AppLocalizations.of(context)!.notificationText),
-                      Switch(
-                          value: notificationState,
-                          onChanged: (newValue) {
-                            setState(() => notificationState = newValue);
-                          })
-                    ],
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Divider(
-                        color: Colors.black,
-                        height: 20,
-                        thickness: lineThickness,
-                        indent: 20,
-                        endIndent: 10),
-                  ),
-                ],
+              // dark theme
+              Container(
+                margin: const EdgeInsets.only(top: 20.0),
+                child: SwitchListTile(
+                  title: Text(AppLocalizations.of(context)!.themeText),
+                  value: darkThemeState,
+                  shape: const Border(
+                      bottom:
+                          BorderSide(color: Colors.black, width: borderWidth)),
+                  onChanged: (value) {
+                    setState(() => darkThemeState = value);
+                  },
+                ),
               ),
             ],
           ),
