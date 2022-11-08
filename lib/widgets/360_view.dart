0f74ae321428278d7b1ed77414dbba4da7af742d
Alessandro Riva <alex27riva@gmail.com>
2022-11-08 11:21:40 +0100

    Gyroscope in 360 view

--- lib/widgets/360_view.dart
+++ lib/widgets/360_view.dart
@@ -2,16 +2,36 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:panorama/panorama.dart';
 
-class View360 extends StatelessWidget {
+class View360 extends StatefulWidget {
   final String imageUrl;
 
   const View360({Key? key, required this.imageUrl}) : super(key: key);
 
   @override
+  State<View360> createState() => _View360State();
+}
+
+class _View360State extends State<View360> {
+  double _lon = 0;
+  double _lat = 0;
+  double _tilt = 0;
+
+  void onViewChanged(longitude, latitude, tilt) {
+    setState(() {
+      _lon = longitude;
+      _lat = latitude;
+      _tilt = tilt;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Panorama(
-      child: Image(image: CachedNetworkImageProvider(imageUrl)),
+      animSpeed: 1.0,
+      sensorControl: SensorControl.Orientation,
+      onViewChanged: onViewChanged,
+      child: Image(image: CachedNetworkImageProvider(widget.imageUrl)),
     ));
   }
 }
