b9ecab1ef13d8c7126742a2cdc3623185cae7cc7
Alessandro Riva <alex27riva@gmail.com>
2022-11-24 13:12:37 +0100

    Rebased dashboard

---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,76 @@
+/*
+ * ar_webview.dart
+ * orino_smart_village
+ * Created by Alessandro Riva
+ * Copyright (c) 2022.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  final GlobalKey webViewKey = GlobalKey();
+
+  InAppWebViewController? webViewController;
+  InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
+      crossPlatform: InAppWebViewOptions(
+        useShouldOverrideUrlLoading: true,
+        mediaPlaybackRequiresUserGesture: false,
+      ),
+      android: AndroidInAppWebViewOptions(
+        useHybridComposition: true,
+      ),
+      ios: IOSInAppWebViewOptions(
+        allowsInlineMediaPlayback: true,
+      ));
+
+  String url = "";
+  double progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Stack(
+      children: [
+        InAppWebView(
+          key: webViewKey,
+          initialUrlRequest: URLRequest(
+              url: Uri.parse(
+                  "http://localhost:8080/assets/webcontent/dashboard/hv_layout.html")),
+          initialOptions: options,
+          onWebViewCreated: (controller) {
+            webViewController = controller;
+          },
+          androidOnPermissionRequest: (controller, origin, resources) async {
+            return PermissionRequestResponse(
+                resources: resources,
+                action: PermissionRequestResponseAction.GRANT);
+          },
+          onProgressChanged: (controller, progress) {
+            setState(() {
+              this.progress = progress / 100;
+            });
+          },
+        ),
+        progress < 1.0 ? LinearProgressIndicator(value: progress) : Container(),
+      ],
+    ));
+  }
+}
