46028c890d07b08d3ad6e99ac5a8a68d3a7166ed
Alessandro Riva <alex27riva@gmail.com>
2022-12-07 16:06:37 +0100

    Scrollable page

--- lib/pages/rocca.dart
+++ lib/pages/rocca.dart
@@ -23,74 +23,76 @@ class Rocca extends StatelessWidget {
         appBar: AppBar(
           title: Text(AppLocalizations.of(context)!.fortressOfOrino),
         ),
-        body: Container(
-          margin: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.only(top: 50.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(15.0),
-                  child: const Image(
-                    image: AssetImage(ImageConstants.roccaTramonto),
+        body: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.only(top: 50.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15.0),
+                    child: const Image(
+                      image: AssetImage(ImageConstants.roccaTramonto),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 80),
-                child: RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                      text: AppLocalizations.of(context)!.fortressDescription,
-                      style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                          leadingDistribution: TextLeadingDistribution.even,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.normal,
-                          decoration: TextDecoration.none),
-                    )),
-              ),
-              const PanoramicButton(
-                  panoramicImageUrl: ImageConstants.roccaPanoramic),
-              // Show on map button container
-              Container(
-                margin: const EdgeInsets.only(left: 20.0, right: 20.0),
-                child: OutlinedButton(
-                    style: OutlinedButton.styleFrom(
-                      backgroundColor: const Color(0xffDDDDDD),
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20),
+                Container(
+                  margin: const EdgeInsets.only(top: 80),
+                  child: RichText(
+                      textAlign: TextAlign.center,
+                      text: TextSpan(
+                        text: AppLocalizations.of(context)!.fortressDescription,
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                            leadingDistribution: TextLeadingDistribution.even,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.normal,
+                            decoration: TextDecoration.none),
+                      )),
+                ),
+                const PanoramicButton(
+                    panoramicImageUrl: ImageConstants.roccaPanoramic),
+                // Show on map button container
+                Container(
+                  margin: const EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        backgroundColor: const Color(0xffDDDDDD),
+                        shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20),
+                          ),
                         ),
                       ),
-                    ),
-                    onPressed: () {
-                      final Uri url = Uri.parse(roccaLocation);
-                      launchUrl(url);
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const FaIcon(
-                          FontAwesomeIcons.mapLocationDot,
-                          size: 20,
-                          color: Color(0xffE04D4D),
-                        ),
-                        //Spacer(),
-                        Text(
-                          AppLocalizations.of(context)!.showOnMapButton,
-                          style: const TextStyle(
-                              color: Colors.black,
-                              fontSize: 13,
-                              fontWeight: FontWeight.normal,
-                              decoration: TextDecoration.none),
-                        )
-                      ],
-                    )),
-              )
-            ],
+                      onPressed: () {
+                        final Uri url = Uri.parse(roccaLocation);
+                        launchUrl(url);
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const FaIcon(
+                            FontAwesomeIcons.mapLocationDot,
+                            size: 20,
+                            color: Color(0xffE04D4D),
+                          ),
+                          //Spacer(),
+                          Text(
+                            AppLocalizations.of(context)!.showOnMapButton,
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 13,
+                                fontWeight: FontWeight.normal,
+                                decoration: TextDecoration.none),
+                          )
+                        ],
+                      )),
+                )
+              ],
+            ),
           ),
         ));
   }
