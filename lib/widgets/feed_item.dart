de217013fe3eb6918194ffd29143f8c4e79185bc
Alessandro Riva <alex27riva@gmail.com>
2023-02-13 13:47:31 +0100

    Add "show more" button in feed_item

--- lib/widgets/feed_item.dart
+++ lib/widgets/feed_item.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:orino_smart_village/models/post.dart';
 import 'package:orino_smart_village/pages/webview.dart';
 import 'package:orino_smart_village/utils/utils.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 Color cardColor = Colors.white;
 const int postTitleMaxLen = 20;
@@ -21,33 +22,22 @@ class FeedItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => WebViewPage(
-              url: post.link.toString(),
-              title: Utils.unescape(post.title),
-            ),
-          ),
-        );
-      },
-      child: Card(
-        margin: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 24.0),
-        elevation: 2,
-        color: cardColor,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16.0),
-        ),
-        // Card title
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Card image
-            SizedBox(
-              width: double.infinity,
-              child: (post.hasImageAvailable)
-                  ? ClipRRect(
+    return Card(
+      margin: const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 24.0),
+      elevation: 2,
+      color: cardColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      // Card title
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Card image
+          SizedBox(
+            width: double.infinity,
+            child: (post.hasImageAvailable)
+                ? ClipRRect(
                     borderRadius: BorderRadius.circular(16.0),
                     child: Image.network(
                       post.featuredImage,
@@ -55,49 +45,66 @@ class FeedItem extends StatelessWidget {
                       height: 150,
                     ),
                   )
-                  : Container(), // empty container
+                : Container(), // empty container
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 16.0,
+              left: 24.0,
+              right: 24.0,
             ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 16.0,
-                left: 24.0,
-                right: 24.0,
+            child: Text(
+              Utils.shortText(
+                Utils.unescape(post.title),
+                maxLength: postTitleMaxLen,
               ),
-              child: Text(
-                Utils.shortText(
-                  Utils.unescape(post.title),
-                  maxLength: postTitleMaxLen,
-                ),
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.black87,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-            ),
-            // Card content
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 8.0,
-                bottom: 16.0,
-                left: 24.0,
-                right: 24.0,
-              ),
-              child: Text(
-                Utils.shortText(Utils.stripHtmlTags(post.excerpt),
-                    maxLength: postContentMaxLen),
-                style: const TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.black87,
-                  fontWeight: FontWeight.w500,
-                ),
-                softWrap: true,
-                maxLines: 8,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(
+                fontSize: 16.0,
+                color: Colors.black87,
+                fontWeight: FontWeight.w700,
               ),
             ),
-          ],
-        ),
+          ),
+          // Card content
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 8.0,
+              bottom: 16.0,
+              left: 24.0,
+              right: 24.0,
+            ),
+            child: Text(
+              Utils.shortText(Utils.stripHtmlTags(post.excerpt),
+                  maxLength: postContentMaxLen),
+              style: const TextStyle(
+                fontSize: 14.0,
+                color: Colors.black87,
+                fontWeight: FontWeight.w500,
+              ),
+              softWrap: true,
+              maxLines: 8,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 16.0,
+            ),
+            child: TextButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => WebViewPage(
+                      url: post.link.toString(),
+                      title: Utils.unescape(post.title),
+                    ),
+                  ),
+                );
+              },
+              child: Text(AppLocalizations.of(context)!.showMore),
+            ),
+          ),
+        ],
       ),
     );
   }
